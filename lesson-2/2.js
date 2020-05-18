fb30e649980115df8af234f979dfd140c664a818
ZhuravlevS <65179272+ZhuravlevS@users.noreply.github.com>
2020-05-18 13:59:37 +0300

    finish

---
+++ lesson-2/2.js
@@ -0,0 +1,25 @@
+// Перепишите `if..else` с использованием нескольких операторов `?`. 
+// Для читаемости — оформляйте код в несколько строк.
+
+////////////////// Задание //////////////////
+// var message;
+
+// if (login == 'Pitter') {
+//   message = 'Hi';
+// } else if (login == 'Owner') {
+//   message = 'Hello';
+// } else if (login == '') {
+//   message = 'unknown';
+// } else {
+//   message = '';
+// }
+
+////////////////// Решение //////////////////
+var message;
+let login = 'Pitter';
+
+message =
+    (login == 'Pitter') ? 'Hi' :
+    (login == 'Owner') ? 'Hello' :
+    (login == '') ?'unknown' : '';
+console.log(message);
